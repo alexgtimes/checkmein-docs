7933371c98c1c0c85c147a9f1c64c0ab652483ba
alex <143785637+alexgtimes@users.noreply.github.com>
2024-06-15 09:29:21 +0930

    update index.js i guess?

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,11 +12,7 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
+      <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
