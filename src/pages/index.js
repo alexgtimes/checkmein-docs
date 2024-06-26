2916bd9e00d26403a8fdec69b6fe0f567b27b44c
alex <143785637+alexgtimes@users.noreply.github.com>
2024-06-26 18:46:34 +0930

    Create index.js

---
+++ src/pages/index.js
@@ -0,0 +1,31 @@
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+
+import Heading from '@theme/Heading';
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+      <div className="container">
+      <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro">
+            Get Started
+          </Link>
+        </div>
+      </div>
+  );
+}
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+      <HomepageHeader />
+    </Layout>
+  );
+}
